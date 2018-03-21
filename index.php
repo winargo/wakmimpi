e5f64a0dc98e362f8bdd8b418c339445f0bfc219
winargo <rwinargo97@gmail.com>
2018-03-21 23:18:17 +0700

    3/21/2018

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
     <body>
         <div class="header">
              <div class="header-container">
-              <img class="Logo-format" src="Images/logo.PNG">
+              <img class="Logo-format" src="http://waktogel.net/assets/img/hf/logo.png">
               <form class="initiate-session" method="post">
                   <input class="form-control" id="username-form-control" type="text" placeholder="Username">
                   <input class="form-control" id="password-form-control" type="password" placeholder="Password">
@@ -53,22 +53,22 @@
                         <div class="col-md-3 left-nav">
                             <div class="row" id="nav-sizing">
                                 <a href="./pages/how_to_play.php"><div class="col-md-12 menu">
-                                    <h4><span class="glyphicon glyphicon-info-sign"></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Cara Bermain</h4>
+                                    <h4><span class="glyphicon glyphicon-info-sign"></span>&nbsp;&nbsp;&nbsp;Cara Bermain</h4>
                                 </div></a>
                                 <a href="http://waktogel.net/hasil_lengkap.php"><div class="col-md-12 menu">
-                                    <h4><span class="glyphicon glyphicon-stats"></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;History Nomor</h4>
+                                    <h4><span class="glyphicon glyphicon-stats"></span>&nbsp;&nbsp;&nbsp;History Nomor</h4>
                                 </div></a>
                                 <a href="./pages/buku_mimpi.php"><div class="col-md-12 menu">
-                                    <h4><span class="glyphicon glyphicon-menu-hamburger"></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Buku Mimpi</h4>
+                                    <h4><span class="glyphicon glyphicon-menu-hamburger"></span>&nbsp;&nbsp;&nbsp;Buku Mimpi</h4>
                                 </div></a>
                                 <a href="./pages/refferal.php"><div class="col-md-12 menu">
-                                    <h4><span class="glyphicon glyphicon-user"></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Refferal</h4>
+                                    <h4><span class="glyphicon glyphicon-user"></span>&nbsp;&nbsp;&nbsp;Refferal</h4>
                                 </div></a>
                                 <a href="./pages/help.php"><div class="col-md-12 menu">
-                                    <h4><span class="glyphicon glyphicon-comment"></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Bantuan</h4>
+                                    <h4><span class="glyphicon glyphicon-comment"></span>&nbsp;&nbsp;&nbsp;Bantuan</h4>
                                 </div></a>
                                 <a href="./pages/lupa_password.php"><div class="col-md-12 menu">
-                                    <h4><span class="glyphicon glyphicon-calendar"></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;lupa Password</h4>
+                                    <h4><span class="glyphicon glyphicon-calendar"></span>&nbsp;&nbsp;&nbsp;lupa Password</h4>
                                 </div></a>
                                 <a href="./pages/lupa_password.php">
                                     <button class="col-md-12 daftar">Dafter</button>
@@ -99,11 +99,11 @@
                                 <!-- Wrapper for slides -->
                                 <div class="carousel-inner">
                                   <div class="item active">
-                                    <img src="Images/banner.jpg" alt="Los Angeles" style="width:100%;">
+                                    <img src="https://s9.postimg.org/3ty7mxysf/WAKTOGEL-01.png" width="840px" height="482">
                                   </div>
 
                                   <div class="item">
-                                    <img src="Images/banner2.jpg" alt="Chicago" style="width:100%;">
+                                    <img src="https://s9.postimg.org/k3yy3esqn/WAKTOGEL-2.png" width="840px" height="482">
                                   </div>
 
                                   <div class="item">
@@ -187,16 +187,14 @@
               
          <!--     -->
     </div>
-    <script src="https://secure.livechatinc.com/licence/9238010/v2/get_dynamic_config.js?t=1521565531025&amp;referrer=&amp;url=http%3A%2F%2Fwaktogel.net%2F&amp;params=&amp;jsonp=__lc_data_214951"></script>
-    <script src="https://secure.livechatinc.com/licence/9238010/v2/get_static_config.0.149.2.2.252.26.65.2.2.2.2.2.438.js?&amp;jsonp=__lc_data_static_config"></script>
-    <script src="https://secure.livechatinc.com/licence/9238010/v2/localization.id.0.08e34bc77e04532ebc1573440c8ea5b3_270dafc139e68db198cadd94938d83ba.js?jsonp=__lc_lang"></script>
-    <div id="livechat-compact-container" style="position: fixed; bottom: 0px; right: 15px; width: 250px; height: 53px; overflow: hidden; visibility: visible; z-index: 2147483639; background: transparent; border: 0px; transition: transform 0.2s ease-in-out; backface-visibility: hidden; opacity: 1; transform: translateY(0%);"><iframe src="about:blank" id="livechat-compact-view" style="position: relative;top: 20px;left: 0;width: 100%;border: 0;padding: 0;margin: 0;float: none;background: none" scrolling="no" frameborder="0" allowtransparency="true" title="LiveChat Minimized Widget"></iframe><a id="livechat-badge" href="#" onclick="LC_API.show_full_view({source:'minimized click'});return false" style="position: absolute; display: block; visibility: hidden; height: 16px; line-height: 16px; background: rgb(255, 0, 0); color: rgb(255, 255, 255); font-size: 10px; text-decoration: none; width: 16px; border-radius: 50%; box-shadow: none; border-top: 0px; padding: 0px; text-align: center; font-family: Lato, sans-serif; top: 12px; left: 20px; opacity: 0;"></a></div>
-    <div id="livechat-eye-catcher" onmouseover="var els = this.getElementsByTagName(&quot;a&quot;); if (els.length) els[0].style.display = &quot;block&quot;;" onmouseout="var els = this.getElementsByTagName(&quot;a&quot;); if (els.length) els[0].style.display = &quot;none&quot;;" style="position: fixed; right: 12px; bottom: 26px; visibility: visible; z-index: 2147483639; background: transparent; border: 0px; padding: 10px 10px 0px 0px; float: left; margin-right: -10px; backface-visibility: hidden; opacity: 1;">	
-    	<a href="#" onclick="LC_API.hide_eye_catcher();return false" style="position: absolute; display: none; top: -5px; right: -5px; padding: 2px 7px; text-decoration: none; color: rgb(0, 0, 0); font-size: 20px; font-family: Arial, sans-serif;" onmouseover="this.style.color='#666'" onmouseout="this.style.color='#000'">×</a>		<a href="#" onclick="LC_API.open_chat_window({source:'eye catcher'});return false" style="display:block" id="livechat-eye-catcher-img"><img src="//cdn.livechatinc.com/cloud/?uri=http://livechat.s3.amazonaws.com/9238010/0/ec/a979db23a9dbea93a3e66ae575d5cc9b.png" style="border:0;display:block;" alt=""></a></div>
-    <div id="livechat-full" style="position: fixed; bottom: 0px; right: 15px; width: 400px; height: 450px; overflow: hidden; visibility: visible; z-index: 2147483639; background: transparent; border: 0px; transition: transform 0.2s ease-in-out; backface-visibility: hidden; transform: translateY(0%); opacity: 1;"><iframe src="https://secure.livechatinc.com/licence/9238010/open_chat.cgi?groups=0&amp;embedded=1&amp;newWebserv=undefined&amp;__lc_vv=2&amp;session_id=S1520696022.6e1c7014dc&amp;server=secure.livechatinc.com#http://waktogel.net/" id="livechat-full-view" name="livechat-full-view" title="LiveChat Widget" scrolling="no" frameborder="0" allowtransparency="true" style="position: absolute; top: 0px; right: 0px; bottom: 0px; left: 0px; width: 100%; height: 100%; border: 0px; padding: 0px; margin: 0px; float: none; background: none;" __idm_id__="610256897"></iframe></div>
-    <div id="lc_invite_layer" style="text-align: left; display: none; z-index: 16777261;"></div>
-    <div id="lc_overlay_layer" style="background-color: rgb(0, 0, 0); position: fixed; left: 0px; top: 0px; z-index: 16777260; display: none; width: 100%; height: 100%;"></div>
-    
-    <div id="lc_overlay_layer" style="background-color: rgb(0, 0, 0); position: fixed; left: 0px; top: 0px; z-index: 16777260; display: none; width: 100%; height: 100%;"></div>
+    <script type="text/javascript">
+window.__lc = window.__lc || {};
+window.__lc.license = 9238010;
+(function() {
+  var lc = document.createElement('script'); lc.type = 'text/javascript'; lc.async = true;
+  lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(lc, s);
+})();
+</script>
     </body>
 </html>
