14df54a1bd267c9b831d1528f1c1daaa7dbd8eb6
winargo <rwinargo97@gmail.com>
2018-03-26 00:30:54 +0700

    slot + homepage

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <html>
    <head>
+    
+    
      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -9,7 +11,7 @@
 
       
       
-       <title>Wakmimpi : Situs Agen Judi Togel Online Singapore Terpercaya</title>
+       <title>Wakmimpi : Situs Agen Judi Tebak Nomor Online Singapore Terpercaya</title>
        
    <script class="cssdeck" src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
    <script type="text/javascript" src="js/header.js"></script>
@@ -25,9 +27,18 @@
             var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(lc, s);
         })();
     </script>
+   <script src="http://wakmimpi.com/assets/js/jquery.jSlots.min.js" type="text/javascript" charset="utf-8"></script>
+  
+  
   
    </head>
     <body>
+        <script src="./js/jquery-1.11.3.js"></script>
+        <script src="./js/jquery.easing.1.3.js"></script>
+        <script src="./js/ezslots.js"></script>
+    
+        <link href="./css/ezslots.css" rel="stylesheet" type="text/css" />
+       
         <div class="header">
              <div class="header-container">
               <img class="Logo-format" src="http://waktogel.net/assets/img/hf/logo.png">
@@ -38,7 +49,7 @@
               </form>
               </div>
         </div>
-           <div class="Content">
+           <div class="Content widthauto">
            <div class="content-forcer">
              
             </div>
@@ -68,18 +79,15 @@
                                     <h4><span class="glyphicon glyphicon-comment"></span>&nbsp;&nbsp;&nbsp;Bantuan</h4>
                                 </div></a>
                                 <a href="./pages/lupa_password.php"><div class="col-md-12 menu">
-                                    <h4><span class="glyphicon glyphicon-calendar"></span>&nbsp;&nbsp;&nbsp;lupa Password</h4>
+                                    <h4><span class="glyphicon glyphicon-calendar"></span>&nbsp;&nbsp;&nbsp;Lupa Password</h4>
                                 </div></a>
                                 <a href="./pages/lupa_password.php">
-                                    <button class="col-md-12 daftar">Dafter</button>
+                                    <button class="btn btn-primary daftar">Daftar</button>
                                 </a>
                             </div>
-                            <div class="container">
-                                <div class="col-md-12 keberuntungan">
-                                    <h3>ANGKA KEBERUNTUNGAN ANDA HARI INI</h3>
-                                </div>
-                            </div>
-                            <div class="container">
+                            <div class="container afterdaftar">
+                               <div id="ezslots1"></div>
+                                <button class="button_random" id="gogogo1">LUCKY NUMBER</button>
                                 <a href="#"><div class="col-md-12"> <img src="./Images/banner2.jpg" alt="togel" class="img-responsive"> </div></a>
                                 <a href="#"><div class="col-md-12"> <img src="./Images/banner.jpg" alt="togel" class="img-responsive"> </div></a>
                             </div>
@@ -99,6 +107,7 @@
                                 <!-- Wrapper for slides -->
                                 <div class="carousel-inner">
                                   <div class="item active">
+                                  
                                     <img src="https://s9.postimg.org/3ty7mxysf/WAKTOGEL-01.png" width="840px" height="482">
                                   </div>
 
@@ -187,14 +196,75 @@
               
          <!--     -->
     </div>
+    
+        <div class="col-md-bottom">
+         </div>
+    <div class="row sometext">
+          <div class="widget">
+                        <p>
+                        </p><h1> Daftar Bandar Agen Tebak Nomor Online Terpercaya, dan Terbesar 2018</h1>
+                WakMimpi merupakan situs judi online terpercaya, terbesar dan terbaik di sepanjang tahun 2018 yang fokus di permainan Tebak Nomor online. Dengan menjadi member WakMimpi, anda juga akan mendapatkan bocoran nomor togel, prediksi angka Tebak Nomor dan info Tebak Nomor Singapura, Hongkong. Keamananan dan kerahasian data member kami yang sangat terjaga melalui enkripsi tingkat tinggi telah menobatkan kami sebagai Bandar Agen Tebak Nomor Online Terpercaya 2018. <a href="www.waktoto.com/">WakMimpi</a> <a href="www.waktoto.com/">Togel</a> <a href="www.waktoto.com/">Togel Online</a>
+                <a href="www.waktoto.com/">Togel Singapura</a> <a href="www.waktoto.com/">Togel Hongkong</a> <a href="www.waktoto.com/">Agen Togel</a>  <a href="www.waktoto.com/">Daftar Togel</a> <a href="www.waktoto.com/">WakMimpi</a>
+                <a href="www.waktoto.com/">Info Togel</a>
+
+                <h2> Info Situs Bandar Tebak Nomor Online Singapura, Hongkong Terpercaya </h2>
+                WakMimpi menyediakan Customer Support (CS) online 24 Jam non-stop dalam membantu process pemasangan Tebak Nomor melalui situs Tebak Nomor kami, process deposit, penarikan dan hal teknik lainnya. System situs Tebak Nomor online yang termudah, terpercaya, terbesar dan terbaik memberikan kenyamanan bermain sepanjang masa. Proses registrasi member yang cepat dan aman. WakMimpi juga memberikan promo dan bonus yang bisa anda dapatkan langsung dari CS kami melalui LiveChat.
+                <a href="www.waktoto.com/">Info Togel</a> <a href="www.waktoto.com/">Togel Online</a> <a href="www.waktoto.com/">WakMimpi</a> <a href="www.waktoto.com/">Agen Togel</a> <a href="www.waktoto.com/">Terbesar</a>  <a href="www.waktoto.com/">Terbaik</a> <a href="www.waktoto.com/">Terpercaya</a>
+                <a href="www.waktoto.com/">angka jitu</a>
+
+                <h3>  Kelebihan WakMimpi – Tebak Nomor Online Terpercaya, Terbesar, Terbaik </h3>
+                 Kepercayaan komunitas situs Tebak Nomor online membuat WakMimpi menjadi Tebak Nomor web terbaik dan terbesar. Tidak hanya mendapatkan bocoran togel, nomor jitu dan info togel, anda juga akan mendapatkan segala mengenai cara bermain togel, buku mimpi, bonus referral kami.
+                Semua hasil pengeluaran asli dan dalam pengelolahan pools resmi Tebak Nomor singapura, hongkong.
+
+                WakMimpi menjamin 100% keamanan member dalam bermain Tebak Nomor online. Dengan system satu-satunya yang menggunakan system enkripsi tingkat tinggi, anda tidak perlu ragu bergabung dengan kami. Daftar jadi member WakMimpi - bandar Tebak Nomor online terbesar dan nikmatin perbedaan dan rasakan keuntungannya. <a href="http://waktoto.com/hasil_lengkap.php">Nomor Togel</a> 
+                <a href="http://waktoto.com/how-to-play.php">Cara Bermain</a> 
+                <a href="http://waktoto.com/bukumimpi.php">Buku Mimpi</a> 
+                <a href="http://waktoto.com/inforeferral.php">Referral Bonus</a> 
+                <a href="http://waktoto.com/support.php">Support CS</a>  
+                <a href="http://waktoto.com/lupapassword.php">Reset</a> 
+            <p></p>
+          </div>
+        
+      </div>
+      <footer>
+          <div class="copyright">
+                  © Copyright 2014 - 2018 <a class="colortext" href="#">WakMimpi.net</a>. All Rights Reserved.
+        </div>
+      </footer>
+    
+    
     <script type="text/javascript">
-window.__lc = window.__lc || {};
-window.__lc.license = 9238010;
-(function() {
-  var lc = document.createElement('script'); lc.type = 'text/javascript'; lc.async = true;
-  lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(lc, s);
-})();
-</script>
+        window.__lc = window.__lc || {};
+        window.__lc.license = 9238010;
+        (function() {
+          var lc = document.createElement('script'); lc.type = 'text/javascript'; lc.async = true;
+          lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
+          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(lc, s);
+        })();
+        </script>
+         <script>
+
+                                         $(function(){
+                                        //setting up some sample set sof things we can make a slot machine of
+                                        var numbers = ['0','1','2','3','4','5','6','7','8','9'];
+                                        var images = ['<img src="images/seven.png">','<img src="images/banana.png">','<img src="images/cherries.png">','<img src="images/lemon.png">','<img src="images/orange.png">','<img src="images/plum.png">','<img src="images/watermelon.png">'];
+                                        var misc = ['!','K','*','#','@','$'];
+                                        var callbackFunction = function(result){
+                                            alert(JSON.stringify(result));
+                                        }
+
+
+                                        //all parameters except id are optional
+                                        var ezslot0 = new EZSlots("ezslots0");
+                                        //simple numbers based one
+                                        var ezslot1 = new EZSlots("ezslots1",{"reelCount":4,"startingSet":[0,0,0,0],"winningSet":[7,7,7,7],"symbols":numbers,"height":50,"width":50});
+                                        //using images instead, and more reels
+                                        
+                                        $("#gogogo0").click(function(){ console.log(ezslot0.spin());});
+                                        $("#gogogo1").click(function(){ console.log(ezslot1.spin());});
+                                        $("#winwinwin1").click(function(){ console.log(ezslot1.win())});
+                                    });
+
+       </script>
     </body>
 </html>
